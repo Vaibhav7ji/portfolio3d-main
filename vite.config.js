42b9a777934e4d33ba0375e2036d77ab4648c21b
Vaibhav7ji <112853171+Vaibhav7ji@users.noreply.github.com>
2025-05-20 22:36:28 +0530

    Fix build: remove direct main.jsx from HTML

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   assetsInclude: ['**/*.glb'],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
+  build: {
+    outDir: 'dist',
+  },
 })
