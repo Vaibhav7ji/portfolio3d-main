81d14dfe4c6318b2804b4dcc18abdda06beefc6d
Vaibhav7ji <112853171+Vaibhav7ji@users.noreply.github.com>
2025-05-23 14:21:11 +0530

    Initial commit

---
+++ portfolio3d-main/README.md
@@ -0,0 +1,2 @@
+# portfolio3d-main
+ Welcome to my portfolio repository! This file provides an overview of my academic background, internship experience, and personal projects showcased in my interactive 3D portfolio website.
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  assetsInclude: ['**/*.glb'],
+})
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   assetsInclude: ['**/*.glb'],
+//   build: {
+//     outDir: 'dist',
+//   },
+// })
